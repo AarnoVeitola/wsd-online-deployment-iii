b24f96bb9c63b1968c5ea797ea622d7cd1b4dcd0
AarnoVeitola <121627348+AarnoVeitola@users.noreply.github.com>
2023-10-16 10:04:52 +0300

    Added database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { serve } from "./deps.js";
 import { configure, renderFile } from "./deps.js";
+import { sql } from "./database/database.js";
 
 configure({
   views: `${Deno.cwd()}/views/`,
@@ -10,19 +11,27 @@ const responseDetails = {
 };
 
 const data = {
-  visits: 0,
+  count: 0,
 };
 
 const handleRequest = async (request) => {
   const url = new URL(request.url);
-  if (url.pathname === "/visits") {
-    data.visits++;
+  if (url.pathname === "/count") {
+    data.count++;
     return new Response(await renderFile("count.eta", data), responseDetails);
-  } else if (url.pathname === "/meaning") {
-    return new Response("Seeking truths beyond meaning of life, you will find 43.");
-  } else {
-    return new Response("Nothing here yet.");
   }
+
+  if (url.pathname === "/addresses") {
+    const rows = await sql`SELECT COUNT(*) as count FROM addresses`;
+    let rowCount = -42;
+    if (rows.length > 0) {
+      rowCount = rows[0].count;
+    }
+
+    return new Response(`Total rows: ${rowCount}`);
+  }
+
+  return new Response("Hello you!");
 };
 
 serve(handleRequest, { port: 7777 });
