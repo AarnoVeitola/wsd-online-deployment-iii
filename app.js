03986dcb5673166593bc0d4a80ec48f3a40f108d
AarnoVeitola <121627348+AarnoVeitola@users.noreply.github.com>
2023-10-16 11:04:28 +0300

    second submission

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import { serve } from "https://deno.land/std@0.202.0/http/server.ts";
 import { configure, renderFile } from "https://deno.land/x/eta@v2.2.0/mod.ts";
 import * as messageService from "./services/messageService.js";
 
-
 configure({
   views: `${Deno.cwd()}/views/`,
 });
@@ -20,34 +19,29 @@ const redirectTo = (path) => {
   });
 };
 
+const listMessages = async () => {
+  const data = {
+    messages: await messageService.findLastFiveMessages(),
+  };
+
+  return new Response(await renderFile("index.eta", data), responseDetails);
+};
+
 const addMessage = async (request) => {
   const formData = await request.formData();
-
   const sender = formData.get("sender");
   const message = formData.get("message");
 
   await messageService.create(sender, message);
-  
-  return redirectTo("/");
 };
 
-const listMessages = async (request) => {
-  const data = {
-    messages: await messageService.findRecent(),
-  };
-
-  return new Response(await renderFile("index.eta", data), responseDetails);
-}
-
-
 const handleRequest = async (request) => {
-  const url = new URL(request.url);
-  if (url.pathname === "/" && request.method === "GET") {
-    return await listMessages(request);
-  } else if (url.pathname === "/" && request.method === "POST") {
-    return await addMessage(request);
+  if (request.method === "GET") {
+    return await listMessages();
+  } else if (request.method === "POST") {
+    await addMessage(request);
+    return redirectTo("/");
   }
-  return redirectTo("/");
 };
 
 serve(handleRequest, { port: 7777 });
